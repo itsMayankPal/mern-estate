4f2ad9bcedb328a1356cc9a9f1cc1fdbb40b925f
Mayank Pal <mayankpal@Mayanks-MacBook-Air-2.local>
2024-01-17 15:03:59 +0530

    backend created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline">Sign In</li>
+            <li className="text-slate-700 hover:`underline`">Sign In</li>
           </Link>
           <Link to="/sign-up">
             <li className="text-slate-700 hover:underline">Sign Up</li>
