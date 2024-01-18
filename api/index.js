39427f718192df01f56dacc4b34429950f465559
Mayank Pal <mayankpal@Mayanks-MacBook-Air-2.local>
2024-01-18 15:54:17 +0530

    developed user's model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// const mongoose = mongoose();
 
 const app = express();
-
-app.listen(3005, () => console.log("server is running on 3005"));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to Mongo"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3005, () => console.log("server is running on` 3005"));
