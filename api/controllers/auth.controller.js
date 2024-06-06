3ef6165240a6b8edd49da12ff06adc434c746705
Mayank Pal <palmakku8@gmail.com>
2024-06-06 22:24:43 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const test = (req, res) => {
   res.json({
     message: "Sign Up API",
   });
 };
+
+//SIGN UP
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
@@ -23,4 +27,29 @@ export const signup = async (req, res, next) => {
   }
 
   await newUser.save();
+};
+
+//SIGN IN
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credential"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
